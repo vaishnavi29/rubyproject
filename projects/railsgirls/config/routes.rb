c8a1b30af328e6fd885919b41c037ecc8b9a2654
vaishnavi <vaishnaviravindran29@gmail.com>
2015-01-31 17:33:53 +0530

    first app!

--- projects/railsgirls/config/routes.rb
+++ projects/railsgirls/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :ideas
-
+root :to => redirect('/ideas')
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
